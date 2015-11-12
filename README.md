7f3f63483ad64a55ea6b834a2438808429ba55de
Clayton Hottinger <chottinger@Claytons-MacBook-Pro.local>
2015-11-12 14:23:32 -0600

    Convert arrays into Employee objects, create array of Employee objects, write function to evaluate STI of an employee, iterate through new array and console log each STI

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 You are going to write a single JavaScript file to automatically calculate employee STI (‘short term incentive’ or ‘bonus’) for a company.
 
 ## Overview
-The company is providing you with a few sample arrays. These sample arrays contain employee data. 
+The company is providing you with a few sample arrays. These sample arrays contain employee data.
 
 Each array is like a row in a spreadsheet.
 
@@ -15,7 +15,7 @@ The fourth item is their review rating.
 
 ## Instructions
 1. Create an Employee constructor that is capable of holding each employee's data.
-2. Convert each employee into an instance of an Employee object. 
+2. Convert each employee into an instance of an Employee object.
 3. Store each instance in an array called `employees`.
 4. Write a function that evaluates the employees data. (Follow instructions in Evaluating Employee Data)
 5. Iterate over the `employees` array and input each index of the array to your function. `console.log` the results of each iteration.
@@ -34,9 +34,9 @@ See instructions for calculating the STI below.
 - Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
 - Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
 - Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
-- If they have 4 employee numbers, this means they have been with the company for longer than 15 years, 
+- If they have 4 employee numbers, this means they have been with the company for longer than 15 years,
 and should receive an additional 5%.
-- However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%. 
+- However, if their annual income is greater than $65,000, they should have their bonus adjusted down 1%.
 - No bonus can be above 13% total.
 
 You may abstract out the STI calculation into a second function if you like, but this is not mandatory.
--- solo_01.js
+++ solo_01.js
@@ -3,3 +3,86 @@ var jem = ["Jem", "62347", "63500", 4];
 var boo = ["Boo", "11435", "54000", 3];
 var scout = ["Scout", "6243", "74750", 5];
 
+var Employee = function(array) {
+  this.name = array[0];
+  this.number = array[1];
+  this.salary = array[2];
+  this.rating = array[3];
+  this.isLongTerm = function(){
+    if (this.number.length == 4){
+      return 5;
+    } else{
+      return 0;
+    }
+  }
+  this.isIncomeGreater = function(){
+    if (this.salary > 65000){
+      return -1;
+    }else{
+      return 0;
+    }
+  }
+};
+
+
+atticus = new Employee(atticus);
+jem = new Employee(jem);
+boo = new Employee(boo);
+scout = new Employee(scout);
+
+var employees = [atticus, jem, boo, scout];
+
+console.log(employees);
+
+function evaluate(employee){
+  var STI = {};
+  switch (employee.rating){
+    case 0:
+      STI.name = employee.name;
+      STI.percentbonus = 0;
+      STI.adjustedSalary = employee.salary;
+      STI.totalBonus = 0;
+      break;
+    case 1:
+      STI.name = employee.name;
+      STI.percentbonus = 0;
+      STI.adjustedSalary = employee.salary;
+      STI.totalBonus = 0;
+      break;
+    case 2:
+      STI.name = employee.name;
+      STI.percentbonus = 0;
+      STI.adjustedSalary = employee.salary;
+      STI.totalBonus = 0;
+      break;
+    case 3:
+      STI.name = employee.name;
+      STI.percentbonus = (4 + employee.isLongTerm() + employee.isIncomeGreater());
+      STI.adjustedSalary = Math.round(parseInt(employee.salary) + employee.salary * STI.percentbonus / 100);
+      STI.totalBonus = Math.round(employee.salary * STI.percentbonus/100);
+      break;
+    case 4:
+      STI.name = employee.name;
+      STI.percentbonus = (6 + employee.isLongTerm() + employee.isIncomeGreater());
+      STI.adjustedSalary = Math.round(parseInt(employee.salary) + employee.salary * STI.percentbonus / 100);
+      STI.totalBonus = Math.round(employee.salary * STI.percentbonus/100);
+      break;
+    case 5:
+      STI.name = employee.name;
+      STI.percentbonus = (10 + employee.isLongTerm() + employee.isIncomeGreater());
+      STI.adjustedSalary = Math.round(parseInt(employee.salary) + employee.salary * STI.percentbonus / 100);
+      STI.totalBonus = Math.round(employee.salary * STI.percentbonus/100);
+      break;
+  }
+  if (STI.percentbonus > 13){
+    STI.percentbonus = 13;
+    STI.adjustedSalary = Math.round(parseInt(employee.salary) + employee.salary * STI.percentbonus / 100);
+    STI.totalBonus = Math.round(employee.salary * STI.percentbonus/100);
+  }
+
+  return STI;
+}
+
+for (var i = 0; i < employees.length; i++){
+  console.log(evaluate(employees[i]));
+};
